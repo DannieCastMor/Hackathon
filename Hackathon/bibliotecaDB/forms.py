758c36b9292bd1f6ae592bfe499992bf1faf3f66
DannieCastMor <95312886+DannieCastMor@users.noreply.github.com>
2021-12-03 15:32:40 -0500

    Delete Hackathon directory

--- Hackathon/bibliotecaDB/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from bibliotecaDB.models import usuarios
-
-
-class FormularioUsuarios(forms.ModelForm):
-    class Meta:
-        model = usuarios
-        fields = '__all__'
